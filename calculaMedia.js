52489e8d2cd1665d00418174ef42080c33855fbf
brunowigers <bruno.wigers@escola.pr.gov.br>
2026-02-09 17:29:54 +0000

    ok

---
+++ calculaMedia.js
@@ -0,0 +1,19 @@
+let nota1 = 2;
+let nota2 = 6;
+let nota3 = 5;
+let nota4 = 1;
+
+const nomeAluno = "valdeci";
+ let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+ if (media >= 7){
+console.log("o aluno" + nomeAluno + "está aprovado")
+} 
+if (media >= 5 && media < 7){
+console.log("o aluno" + nomeAluno + "está no se-liga")
+ }
+ if (media < 5){
+console.log("o aluno" + nomeAluno + "está reprovado")
+ }
+ console.log("o aluno:" + nomeAluno);
+ console.log("Tem a média:" + media);
