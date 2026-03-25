bb5a649a096d2df571f1031920b1f4b39455ee38
brunowigers <bruno.wigers@escola.pr.gov.br>
2026-03-25 13:03:27 +0000

    ok

---
+++ vetor.js
@@ -0,0 +1,23 @@
+let compras = ["morango","banana","pão","leite"];
+console.log(compras)
+
+//console.log("O primeiro item da lista é" + compras[0]);
+//console.log("O último item da lista é" + compras[3]);
+
+compras[2] = "macarrão";
+console.log(compras[2])
+
+compras.push("pepsi");
+compras.push("limão");
+//console.log (compras)
+
+compras.pop()
+compras.pop()
+compras.pop()
+//console.log (compras)
+
+console.log(compras.length)
+
+
+
+
